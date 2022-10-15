85041fea5cb2d892c7a668def08a1d6b42fc8e8d
Ram4GB <leminhcuong2988@yahoo.com.vn>
2022-10-15 22:31:50 +0700

    default arrow false

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
               speed={500}
               slidesToShow={1}
               slidesToScroll={1}
-              arrows={true}
+              arrows={false}
               autoplay={true}
             >
               <div>
